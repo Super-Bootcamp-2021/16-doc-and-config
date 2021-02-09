ffcc96b45bc183200df21d7a3bd42a49287b303c
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-02-09 20:19:30 +0700

    add documentation API performance

--- tugas/schema/main.js
+++ tugas/schema/main.js
@@ -1,22 +1,11 @@
 const SwaggerUI = require('swagger-ui');
 require('swagger-ui/dist/swagger-ui.css');
-const specTask = require('./task.yaml');
-const specWorker = require('./worker.yaml');
-const specPeformance = require('./peformance.yaml');
+const spec = require('./performance.yaml');
 
-spec.host = 'localhost:9999';
+spec.host = 'localhost:7003';
 
-SwaggerUI({
-  specTask,
-  dom_id: '#swagger1',
-});
 
 SwaggerUI({
-  specWorker,
-  dom_id: '#swagger2',
-});
-
-SwaggerUI({
-  specPeformance,
-  dom_id: '#swagger3',
+  spec,
+  dom_id: '#swagger',
 });

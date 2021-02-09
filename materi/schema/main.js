05476194bc1088242a859f823691f65ed99c0aad
Muhammad Hasan <hasan@sirus.dev>
2021-02-09 10:57:25 +0700

    add todo service schema

--- materi/schema/main.js
+++ materi/schema/main.js
@@ -2,7 +2,7 @@ const SwaggerUI = require('swagger-ui');
 require('swagger-ui/dist/swagger-ui.css');
 const spec = require('./todo.yaml');
 
-spec.host = 'localhost:7001';
+spec.host = 'localhost:9999';
 
 SwaggerUI({
   spec,

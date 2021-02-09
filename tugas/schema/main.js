9cec8315c151aee253aa8c846218e497df4a6ec9
Annas Adharuqudni <57264462+adharuqudni@users.noreply.github.com>
2021-02-09 21:20:51 +0700

    Add schema on task,service, and performance (100%)

--- tugas/schema/main.js
+++ tugas/schema/main.js
@@ -1,10 +1,22 @@
 const SwaggerUI = require('swagger-ui');
 require('swagger-ui/dist/swagger-ui.css');
-const spec = require('./todo.yaml');
+const performance = require('./performance.yaml');
+const task = require('./task.yaml');
+const worker = require('./worker.yaml');
 
-spec.host = 'localhost:9999';
+worker.host = 'localhost:7001';
+task.host = 'localhost:7002';
+performance.host = 'localhost:7003';
 
 SwaggerUI({
-  spec,
-  dom_id: '#swagger',
+  spec: task,
+  dom_id: '#task',
+});
+SwaggerUI({
+  spec: worker,
+  dom_id: '#worker',
+});
+SwaggerUI({
+  spec: performance,
+  dom_id: '#performance',
 });

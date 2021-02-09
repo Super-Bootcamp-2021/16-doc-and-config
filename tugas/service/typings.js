9943170867da8ff8da14fb5253b4766a1fad32ce
fauzana18 <fauzana18@gmail.com>
2021-02-09 20:14:39 +0700

    Task

--- tugas/service/typings.js
+++ tugas/service/typings.js
@@ -6,3 +6,11 @@
  * @property {number} task_cancelled jumlah pekerjaan yang dibatalkan
  * @property {number} total_worker jumlah pekerja yang terdaftar
  */
+
+/**
+ * Task type definition
+ * @typedef {Object} TaskData
+ * @property {string} job nama pekerjaan
+ * @property {number} assignee id dari pekerja yang ditugaskan
+ * @property {Blob} attachment file lampiran dari pekerjaan
+ */

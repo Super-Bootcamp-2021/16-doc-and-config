6e6d5cc38ee149c0dfd0a8330518f21a2d41c902
Inosensius Karelo Hesay <inosensius241099@gmail.com>
2021-02-09 21:41:46 +0700

    API service task + Lib webapp task and service task

--- tugas/webapp/src/tasks/task.client.js
+++ tugas/webapp/src/tasks/task.client.js
@@ -1,19 +1,85 @@
+/**@module Webapp_TaskClient */
+
 const { client } = require('../lib/http-client');
 
 const { SERVICE_BASEURL } = require('./config');
 
+/**
+ * menambahkan pekerjaan baru
+ *
+ * @param {TaskData} task data pekerjaan baru
+ * @returns {Promise<Task>} detail pekerjaan yang sudah disimpan
+ *
+ * @example
+ * exports.add = (data) => async (dispatch) => {
+ *  dispatch(loadingAction());
+ *  try {
+ *    const task = await taskSvc.add(data);
+ *    dispatch(addedAction(task));
+ *  } catch (err) {
+ *    dispatch(errorAction(`gagal menambahkan ${data.job}`));
+ *  }
+ * };
+ */
 function add(data) {
   return client.post(`${SERVICE_BASEURL}/add`, data);
 }
 
+/**
+ * mengambil keseluruhan task yang ada
+ * @returns {Promise<Task[]>} daftar task
+ *
+ * @example
+ * exports.getList = async (dispatch) => {
+ *  dispatch(loadingAction());
+ *  try {
+ *    const tasks = await taskSvc.list();
+ *    dispatch(tasksLoadedAction(tasks));
+ *  } catch (err) {
+ *    dispatch(errorAction('gagal memuat daftar pekerjaan'));
+ *  }
+ * };
+ */
 function list() {
   return client.get(`${SERVICE_BASEURL}/list`);
 }
 
+/**
+ * menandakan pekerjaan telah selesai
+ * @param {number} id id dari pekerjaan
+ * @returns {Promise<TaskCancel>} detail pekerjaan yang sudah disimpan
+ *
+ * @example
+ * exports.cancel = (id) => async (dispatch) => {
+ *  dispatch(loadingAction());
+ *  try {
+ *    await taskSvc.cancel(id);
+ *    dispatch(canceledAction(id));
+ *  } catch (err) {
+ *    dispatch(errorAction('gagal membatalkan pekerjaan'));
+ *  }
+ * };
+ */
 function cancel(id) {
   return client.put(`${SERVICE_BASEURL}/cancel?id=${id}`);
 }
 
+/**
+ * menandakan pekerjaan telah selesai
+ * @param {number} id id dari pekerjaan
+ * @returns {Promise<TaskDone>} detail pekerjaan yang sudah disimpan
+ *
+ * @example
+ * exports.done = (id) => async (dispatch) => {
+ *  dispatch(loadingAction());
+ *  try {
+ *    await taskSvc.done(id);
+ *    dispatch(doneAction(id));
+ *  } catch (err) {
+ *    dispatch(errorAction('gagal menyelesaikan pekerjaan'));
+ *  }
+ * };
+ */
 function done(id) {
   return client.put(`${SERVICE_BASEURL}/done?id=${id}`);
 }
--- tugas/webapp/src/tasks/worker.client.js
+++ tugas/webapp/src/tasks/worker.client.js
@@ -1,7 +1,23 @@
+/**@module Webapp_WorkerClient-InTasks */
 const { client } = require('../lib/http-client');
 
 const { WORKER_SERVICE_BASEURL } = require('./config');
 
+/**
+ * mengambil keseluruhan pekerja yang ada
+ * @returns {Promise<Worker[]>} daftar pekerja
+ *
+ * @example
+ * exports.getWorkersList = async (dispatch) => {
+ *  dispatch(loadingAction());
+ *  try {
+ *    const workers = await workerSvc.list();
+ *    dispatch(workersLoadedAction(workers));
+ *  } catch (err) {
+ *    dispatch(errorAction('gagal membatalkan pekerjaan'));
+ *  }
+ * };
+ */
 function list() {
   return client.get(`${WORKER_SERVICE_BASEURL}/list`);
 }

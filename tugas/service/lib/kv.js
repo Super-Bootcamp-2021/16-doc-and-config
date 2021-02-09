5adec9d45edad1de848d97e60de706610f5f0b8d
Muhammad Achsan Hujjatul Islam <achsanh@gmail.com>
2021-02-09 18:58:09 -0500

    Add Lib DOCS

--- tugas/service/lib/kv.js
+++ tugas/service/lib/kv.js
@@ -1,8 +1,14 @@
+/** @module lib_kv */
 const redis = require('redis');
 const { promisify } = require('util');
 
 let client;
 
+/**
+ * Connect Functions of Redis
+ * @param {*} options additional options of Redis Connection
+ * @see https://github.com/NodeRedis/node-redis#user-content-options-object-properties
+ */
 function connect(options) {
   return new Promise((resolve, reject) => {
     client = redis.createClient(options);
@@ -15,22 +21,38 @@ function connect(options) {
   });
 }
 
+/**
+ * Save Function to save/store data into KV database
+ * @param {String} db key of database
+ * @param {String} data value of database
+ */
 function save(db, data) {
   const setAsync = promisify(client.set).bind(client);
   return setAsync(db, data);
 }
 
+/**
+ * Read Function to read/get value in specific key of KV Database
+ * @param {String} db key of database
+ */
 async function read(db) {
   const getAsync = promisify(client.get).bind(client);
   const val = await getAsync(db);
   return JSON.parse(val);
 }
 
+/**
+ * Drop Function to drop/delete key from KV Database
+ * @param {String} db key of database
+ */
 function drop(db) {
   const delAsync = promisify(client.del).bind(client);
   return delAsync(db);
 }
 
+/**
+ * Close Connections of Redis
+ */
 function close() {
   if (!client) {
     return;

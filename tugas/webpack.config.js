c23251c59a1737ddbeaacc3d61719c85ea78b06a
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-02-09 20:08:48 +0700

    membuat schema API worker-service: POST http://localhost:7001/register

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -1,25 +1,37 @@
+const DotenvWebpackPlugin = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
   entry: {
-    tasks: './webapp/src/tasks/main.js',
+    //tasks: './webapp/src/tasks/main.js',
     worker: './webapp/src/worker/main.js',
-    performance: './webapp/src/performance/main.js',
+    //performance: './webapp/src/performance/main.js',
+    schema: './schema/main.js'
   },
   output: {
-    path: path.resolve(__dirname, 'www'),
+    path: path.resolve(__dirname, './webapp/www'),
     filename: '[name].js',
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './webapp//www',
+    contentBase: './webapp/www/',
     port: 7000,
   },
+  plugins: [
+    new DotenvWebpackPlugin({
+      path: './.env',
+      safe: true,
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.yaml$/,
+        use: [{ loader: 'json-loader' }, { loader: 'yaml-loader' }],
       },
     ],
   },

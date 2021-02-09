a8391365ddedefdf8a7a015fd76c6265881c0a1a
Muhammad Achsan Hujjatul Islam <achsan@gmail.com>
2021-02-09 00:50:44 -0800

    Add Config File |-> for database connection

--- tugas/service/main.js
+++ tugas/service/main.js
@@ -7,18 +7,12 @@ const { WorkerSchema } = require('./worker/worker.model');
 const workerServer = require('./worker/server');
 const tasksServer = require('./tasks/server');
 const performanceServer = require('./performance/server');
+const { config } = require('./config');
 
 async function init() {
   try {
     console.log('connect to database');
-    await orm.connect([WorkerSchema, TaskSchema], {
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'sanbercode2',
-    });
+    await orm.connect([WorkerSchema, TaskSchema], config.database);
     console.log('database connected');
   } catch (err) {
     console.error('database connection failed');
